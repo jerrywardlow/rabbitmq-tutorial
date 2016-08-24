1a7ad0e9b01c4b67911a974749fa3c6a67d68d3b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-24 15:20:48 -0700

    Copy missing elements from receive.py

--- worker.py
+++ worker.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 import time
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
 
 def callback(ch, method, proeprties, body):
     print(" [x] Received %r" % body)
     time.sleep(body.count(b'.'))
     print(" [x] Done")
+
+channel.basic_consume(callback,
+                      queue='hello',
+                       no_ack=True)
+
+print(' [*] Waiting for messages, ditch with Ctrl-C')
+channel.start_consuming()
