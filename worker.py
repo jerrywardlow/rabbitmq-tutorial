ad658abf5e4158cf34faa8614ec642ab33053800
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-24 16:02:02 -0700

    QOS prefetch setting

--- worker.py
+++ worker.py
@@ -15,6 +15,7 @@ def callback(ch, method, proeprties, body):
     print(" [x] Done")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
                       queue='hello')
 
