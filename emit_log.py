629acb423f74cd7e0b0461b5b56e963d8b6033c6
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-25 18:26:10 -0700

    Initial commit

---
+++ emit_log.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import pika
+import sys
+
+connection = pika.Blocking Connection(pika.ConnectionParameters(
+    host='localhost'))
+
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message)
+
+print(" ('_') Sent %r" % message)
+connection.close()  
